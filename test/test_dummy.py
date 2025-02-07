566598ae56435cef7fda7ccb804172a0478f7842
Kumaran Rajendhiran <kumaran@airt.ai>
2025-02-07 11:41:58 +0000

    Add failing test for dummy

---
+++ test/test_dummy.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2023 - 2025, AG2ai, Inc., AG2ai open-source projects maintainers and core contributors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from autogen.dummy import add, ag2_ceil
+
+
+def test_add() -> None:
+    actual = add(3, 5)
+    expected = 8
+    assert actual == expected
+    raise ValueError("Dummy raise")
+
+
+def test_ag2_ceil() -> None:
+    actual = ag2_ceil(0.9)
+    expected = 100100101
+    assert actual == expected
